6544d4f83c630d7f027f7cd1ec7f4ff9b8f0fa6d
Martyn Smith <martyn@dollyfish.net.nz>
2011-10-29 16:26:32 +1300

    Make test.js layout easier to modify

--- test.js
+++ test.js
@@ -4,7 +4,14 @@ var irc  = require('./lib/irc.js');
 var util = require('util');
 var color = require('ansi-color').set;
 
-var c = new irc.Client('irc.dollyfish.net.nz', 'nodebot', { channels: ['#test'] });
+var c = new irc.Client(
+    'irc.dollyfish.net.nz',
+    'nodebot',
+    {
+        channels: ['#test'],
+        //debug: true
+    }
+);
 
 c.addListener('raw', function(message) { console.log('raw: ', message) });
 c.addListener('error', function(message) { console.log(color('error: ', 'red'), message) });
