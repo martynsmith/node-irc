c0e3acf302b3229a7a055015e0e2323cd4c7d123
Justin Gallardo <justin.gallardo@gmail.com>
2015-01-06 01:46:02 -0800

    fix(lint): Use single quotes everywhere consistently

--- example/bot.js
+++ example/bot.js
@@ -24,10 +24,10 @@ bot.addListener('message', function(from, to, message) {
             bot.say(to, 'Hello there ' + from);
         }
         if (message.match(/dance/)) {
-            setTimeout(function() { bot.say(to, "\u0001ACTION dances: :D\\-<\u0001"); }, 1000);
-            setTimeout(function() { bot.say(to, "\u0001ACTION dances: :D|-<\u0001");  }, 2000);
-            setTimeout(function() { bot.say(to, "\u0001ACTION dances: :D/-<\u0001");  }, 3000);
-            setTimeout(function() { bot.say(to, "\u0001ACTION dances: :D|-<\u0001");  }, 4000);
+            setTimeout(function() { bot.say(to, '\u0001ACTION dances: :D\\-<\u0001'); }, 1000);
+            setTimeout(function() { bot.say(to, '\u0001ACTION dances: :D|-<\u0001');  }, 2000);
+            setTimeout(function() { bot.say(to, '\u0001ACTION dances: :D/-<\u0001');  }, 3000);
+            setTimeout(function() { bot.say(to, '\u0001ACTION dances: :D|-<\u0001');  }, 4000);
         }
     }
     else {
