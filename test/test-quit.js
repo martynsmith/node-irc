e4000b7a8ac42d9eb16fb6c3f362e1425d664f4b
Justin Gallardo <justin.gallardo@gmail.com>
2016-11-25 16:23:36 -0800

    fix lint errors.

--- test/test-quit.js
+++ test/test-quit.js
@@ -4,32 +4,32 @@ var test = require('tape');
 var testHelpers = require('./helpers');
 
 test('connect and quit with message', function(t) {
-  var client, mock, expected;
+    var client, mock, expected;
 
-  mock = testHelpers.MockIrcd();
-  client = new irc.Client('localhost', 'testbot', {debug: true});
+    mock = testHelpers.MockIrcd();
+    client = new irc.Client('localhost', 'testbot', {debug: true});
 
-  expected = testHelpers.getFixtures('quit');
+    expected = testHelpers.getFixtures('quit');
 
-  t.plan(expected.sent.length + expected.received.length + 1);
+    t.plan(expected.sent.length + expected.received.length + 1);
 
-  mock.server.on('connection', function() {
-    mock.send(':localhost 001 testbot :Welcome to the Internet Relay Chat Network testbot\r\n');
-  });
+    mock.server.on('connection', function() {
+        mock.send(':localhost 001 testbot :Welcome to the Internet Relay Chat Network testbot\r\n');
+    });
 
-  client.on('registered', function() {
-    t.equal(mock.outgoing[0], expected.received[0][0], expected.received[0][1]);
-    client.disconnect('quitting as a test', function() {});
-  });
+    client.on('registered', function() {
+        t.equal(mock.outgoing[0], expected.received[0][0], expected.received[0][1]);
+        client.disconnect('quitting as a test', function() {});
+    });
 
-  mock.on('end', function() {
-    var msgs = mock.getIncomingMsgs();
+    mock.on('end', function() {
+        var msgs = mock.getIncomingMsgs();
 
-    t.equal(msgs.length, expected.sent.length, 'Server received the correct amount of messages.')
+        t.equal(msgs.length, expected.sent.length, 'Server received the correct amount of messages.')
 
-    for (var i = 0; i < msgs.length; i++) {
-      t.equal(msgs[i], expected.sent[i][0], expected.sent[i][1]);
-    }
-    mock.close();
-  });
+        for (var i = 0; i < msgs.length; i++) {
+            t.equal(msgs[i], expected.sent[i][0], expected.sent[i][1]);
+        }
+        mock.close();
+    });
 });
