0c50f16d0aebc4dab11dff5704b844c13045919f
Will Hunt <will@half-shot.uk>
2021-05-17 19:20:52 +0100

    Update test

--- test/test-parse-line.js
+++ test/test-parse-line.js
@@ -1,20 +1,22 @@
-var parseMessage  = require('../lib/parse_message');
-var test = require('tape');
+const { parseMessage } = require('../lib/parse_message');
+const test = require('tape');
 
-var testHelpers = require('./helpers');
+const testHelpers = require('./helpers');
 
 test('irc.parseMessage', function(t) {
-    var checks = testHelpers.getFixtures('parse-line');
+    debugger;
+    const checks = testHelpers.getFixtures('parse-line');
 
     Object.keys(checks).forEach(function(line) {
-        var stripColors = false;
+        console.log("Foo");
+        let stripColors = false;
         if (checks[line].hasOwnProperty('stripColors')) {
             stripColors = checks[line].stripColors;
             delete checks[line].stripColors;
         }
-        t.equal(
-            JSON.stringify(checks[line]),
-            JSON.stringify(parseMessage(line, stripColors)),
+        t.deepEqual(
+            checks[line],
+            parseMessage(line, stripColors),
             line + ' parses correctly'
         );
     });
